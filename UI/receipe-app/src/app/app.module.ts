8ae83199980c98d13266ab4049f555f580aaced6
Sarah Bayrakdar <sarah.beirkdar@gmail.com>
2019-06-23 21:26:58 +0400

    Add UI :)

---
+++ UI/receipe-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

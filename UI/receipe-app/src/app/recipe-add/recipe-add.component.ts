58dffd76624f729c07829ca27c68acbef7bbe747
Sarah Bayrakdar <sarah.beirkdar@gmail.com>
2020-04-08 13:34:45 +0400

    add ng-bootstrap

--- UI/receipe-app/src/app/recipe-add/recipe-add.component.ts
+++ UI/receipe-app/src/app/recipe-add/recipe-add.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ICategory } from '../recipe-list/category';
 import { RecipeService } from '../recipe-list/recipe.service';
-// import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-recipe-add',
@@ -11,8 +11,27 @@ import { RecipeService } from '../recipe-list/recipe.service';
 export class RecipeAddComponent implements OnInit {
   categories : ICategory[] = [];
   errorMessage : string;
-  constructor(private recipeService : RecipeService) { }
+  closeResult = '';
+  constructor(private recipeService : RecipeService,private modalService: NgbModal) { }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  
   ngOnInit() {
     this.recipeService.getCategories().subscribe(
       categories => this.categories = categories,
